958c380053510fc4f874007806f7fc2cd7f4d1c5
CarlosD <75714017+Blackshadow4c@users.noreply.github.com>
2020-12-15 18:35:05 -0600

    Carlos is the best

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PlaygroundAlma from '../containers/PlaygroundAlma';
+import PlaygroundCarlos from '../containers/PlaygroundCarlos';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/alma" component={PlaygroundAlma} />
+      <Route exact path="/carlos" component={PlaygroundCarlos} />
     </Switch>
   </BrowserRouter>
 );
