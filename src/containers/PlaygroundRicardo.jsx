6658c25b2ac3578c94f2b39aa04f9add02ee0077
MordredAlter <39255157+MordredAlter@users.noreply.github.com>
2020-12-15 18:41:43 -0600

    Rick el poderoso

---
+++ src/containers/PlaygroundRicardo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlaygroundRicardo = () => (
+  <div>
+    El poderoso Rick
+  </div>
+);
+
+export default PlaygroundRicardo;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PlaygroundAlma from '../containers/PlaygroundAlma';
+import PlaygroundRicardo from '../containers/PlaygroundRicardo';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/alma" component={PlaygroundAlma} />
+      <Route exact path="/ricardo" component={PlaygroundRicardo} />
     </Switch>
   </BrowserRouter>
 );
