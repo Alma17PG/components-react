9b3dc500c2bffbab833142fbdc5a3898ac632523
MordredAlter <39255157+MordredAlter@users.noreply.github.com>
2020-12-16 19:36:47 -0600

    Component-Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './input.css';
+
+const InputRick = () => (
+  <div className="reactbox">
+    <form className="react">
+      <input type="text" className="text" placeholder="Type your email" />
+      <input type="submit" className="btn" value="Subscribe" />
+    </form>
+  </div>
+);
+
+export default InputRick;
