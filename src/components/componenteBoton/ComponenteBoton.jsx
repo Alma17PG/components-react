b3e68314c7cbc4593f527f32fa028efc8cb390bd
martinjuarez <50249064+martinjuarez@users.noreply.github.com>
2020-12-16 19:42:13 -0600

    Hasta el momento

---
+++ src/components/componenteBoton/ComponenteBoton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const ComponenteBoton = () => (
+    <div>
+        <button className ="ComponenteBoton"> Hire me </button>
+    </div>
+);
+
+export default ComponenteBoton;
