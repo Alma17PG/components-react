70d73a993ccd3e6ff52c69bb7e0a448177d3767c
Alma17PG <48005223+Alma17PG@users.noreply.github.com>
2020-12-18 17:55:56 -0600

    Adding component props state

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from '../componenteBoton/ComponenteBoton';
 
 import './style.css';
 
-const ProgressBar = () => {
-  const percentage = 25;
+const ProgressBar = (props) => {
+  const [percentage, setPercentage] = useState(0);
+
   return (
     <div className="root">
       <div className="progress-bar-back">
@@ -12,6 +15,7 @@ const ProgressBar = () => {
       <div className="progress-bar-percentage">
         {`${percentage}%`}
       </div>
+      <Button onClick={() => setPercentage(percentage + 10)} />
     </div>
   );
 };
