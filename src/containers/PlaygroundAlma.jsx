cc60d618ff7f9f72b544ba9eccc6c1848aa1fa58
Alma17PG <48005223+Alma17PG@users.noreply.github.com>
2020-12-18 16:44:02 -0600

    Adding ProgressBar component

--- src/containers/PlaygroundAlma.jsx
+++ src/containers/PlaygroundAlma.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import ProgressBar from '../components/ProgressBar';
+
 const PlaygroundAlma = () => (
-  <div>
-    Alma
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <ProgressBar />
   </div>
 );
 
