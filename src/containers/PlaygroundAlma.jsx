c48612b0c0c32347425bb54c08fbcf36bb7e4904
Eduardo <lloydice@gmail.com>
2020-12-21 17:28:44 -0600

    Adding javascript functions

--- src/containers/PlaygroundAlma.jsx
+++ src/containers/PlaygroundAlma.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-import ProgressBar from '../components/ProgressBar';
+import Calendario from '../components/Calendario';
 
-const PlaygroundAlma = () => (
+const days = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const PlaygroundAlma = () => {
+  days.push(9)
+  console.log(days)
+  console.log([...days, 10])
+  return (
   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <div ><ProgressBar percentage={75} /></div>
+    {
+      days.filter(day => day % 2 === 0).map(day => {
+        return (
+          <Calendario day={day} />
+        )
+      })
+    }
   </div>
-);
+)};
 
 export default PlaygroundAlma;
