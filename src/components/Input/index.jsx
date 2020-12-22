815246faea3fd2e8fbc508315fcad5e0811a61de
MordredAlter <39255157+MordredAlter@users.noreply.github.com>
2020-12-22 15:16:14 -0600

    Componente Card 8 & Input updated

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './index.css';
 
-const InputRick = () => (
-  <div className="reactbox">
-    <form className="react">
-      <input type="text" className="text" placeholder="Type your email" />
-      <input type="submit" className="btn" value="Subscribe" />
-    </form>
-  </div>
-);
+const InputRick = (props) => {
+  const {onClick} = props;
+  return (
+    <div className="reactbox">
+      <form className="react">
+        <input type="text" className="text" placeholder="Type your email" />
+        <input type="submit" className="btn" value="Subscribe" />
+      </form>
+    </div>
+  );
+};
 
 export default InputRick;
--- src/containers/PlaygroundRicardo.jsx
+++ src/containers/PlaygroundRicardo.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import ContadorRick from '../components/ContadorRick';
+import React, { useState } from 'react';
+// import ContadorRick from '../components/ContadorRick';
+import ComponenteBoton from '../components/componenteBoton/ComponenteBoton';
+import ComponentSubscribe from '../components/ComponentSubscribe';
 
-const PlaygroundRicardo = () => (
-  <div>
-    <ContadorRick user="Rick" />
-  </div>
-);
+const PlaygroundRicardo = () => {
+  const [producto, setProducto] = useState('');
+  const [products, setProducts] = useState([]);
+  const onClick = () => {
+    setProducts([...products, producto]);
+    setProducto('');
+  };
+  return (
+    <div>
+      <input type="text" value={producto} onChange={(evento) => setProducto(evento.target.value)} />
+      <ComponenteBoton onClick={() => onClick()} />
+      {
+        products.map((product) => (
+          // eslint-disable-next-line react/jsx-key
+          <div>
+            {product}
+          </div>
+        ))
+      }
+    </div>
+    // <ComponentSubscribe /> 
+  );
+};
 
 export default PlaygroundRicardo;
