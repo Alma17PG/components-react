8fc4fa427498b2222db5f2f3479ff1a8e1fbcb02
MordredAlter <39255157+MordredAlter@users.noreply.github.com>
2020-12-21 18:15:01 -0600

    listaRick

--- src/containers/PlaygroundRicardo.jsx
+++ src/containers/PlaygroundRicardo.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import ContadorRick from '../components/ContadorRick';
+import React, { useState } from 'react';
+// import ContadorRick from '../components/ContadorRick';
+import ComponenteBoton from '../components/componenteBoton/ComponenteBoton';
 
-const PlaygroundRicardo = () => (
-  <div>
-    <ContadorRick user="Rick" />
-  </div>
-);
+const PlaygroundRicardo = () => {
+  const [producto, setProducto] = useState('');
+  const [products, setProducts] = useState([]);
+  const onClick = () => {
+    setProducts([...products, producto]);
+    setProducto('');
+  };
+  return (
+    <div>
+      <input type="text" value={producto} onChange={(evento) => setProducto(evento.target.value)} />
+      <ComponenteBoton onClick={() => onClick()} />
+      {
+        products.map((product) => (
+          <div>
+            {product}
+          </div>
+        ))
+      }
+    </div>
+  );
+};
 
 export default PlaygroundRicardo;
