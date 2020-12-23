f8e5a34e14375d019d61c8e3ff26a531d4fab17a
CarlosD <75714017+Blackshadow4c@users.noreply.github.com>
2020-12-22 18:31:33 -0600

    Dia Carlos Redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,12 @@
 /* eslint-disable import/prefer-default-export */
-import { SET_USER } from '../constants';
+import { SET_USER, SET_DIA } from '../constants';
 
 export const setUser = (value) => ({
   type: SET_USER,
+  value,
+});
+
+export const setDia = (value) => ({
+  type: SET_DIA,
   value,
 });
